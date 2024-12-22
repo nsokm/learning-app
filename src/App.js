39b4505f72db87fa88c3eb16b9b5e96c18c2331a
ns_okm <saetiewnw@gmail.com>
2024-12-22 13:43:42 +0700

    test code for graph

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './views/Pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      {/* <header className="App-header">
+      
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home.css'
+
+const Home = (props) => {
+
+
+  return (
+    <div className="Container">
+      <header className='header'>
+        <p>
+          Welcome
+        </p>
+      </header>
+    
+    </div>
+  )
+}
+
+export default Home;
